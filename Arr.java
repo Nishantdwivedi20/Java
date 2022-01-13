b02521c29b017bf115adea547ca16334113eae50
Nishantdwivedi20 <93776431+Nishantdwivedi20@users.noreply.github.com>
2022-01-13 22:52:11 +0530

    Create Arr.java

---
+++ Arr.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Hello {
+    public static void main(String[] args)
+    {
+        int arr[]=new arr[5];
+        Scanner s=new Scanner(System.in);
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+         for(int i=0;i<arr.length;i++)
+        {
+         System.out.println(arr[i]);
+        }
+    }
+}
